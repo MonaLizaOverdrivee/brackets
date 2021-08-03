3b7c122b27719b62305e3c333c4181f9a9ec2d19
MonaLizaOverdrivee <santospele@mail.ru>
2021-08-03 15:04:42 +0300

    fix: correct solution

--- src/index.js
+++ src/index.js
@@ -1,31 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-   const pair = [];
-  const [open, close] = bracketsConfig.reduce((acc,[a,b]) => {
-    acc[0].push(a)
-    acc[1].push(b)
-    return acc
-  },[[], []])
-  function configOpen() {
-    let pairNumber = 0;
-    return function(val) {
-      pair[pairNumber] = [val, null]
-      pairNumber++
+  const stack = [];
+  str.split("").forEach((itm) => {
+    const [open, close] = bracketsConfig.find((subitm) => subitm.includes(itm));
+    if (itm === stack[stack.length - 1] && open === close) {
+      stack.pop();
+    } else if (itm === close && stack[stack.length - 1] === open) {
+      stack.pop();
+    } else {
+      stack.push(itm)
     }
-  }
-  function configClose() {
-    let pairNumber = 0
-    return function(val) {
-      pair[pair.length - 1 - pairNumber][1] = val
-      if(pair.length - 1 - pairNumber === 0) pairNumber = 0
-      else pairNumber++
-    }
-  }
-  const setOpen = configOpen()
-  const setClose = configClose()
+  });
 
-  for(let v of str) {
-    if(open.includes(v)) setOpen(v)
-    else setClose(v)
-  }
-  return pair.every(itm => bracketsConfig.some(sub => itm[0] === sub[0] && itm[1] === sub[1]))
+  return !stack.length;
 }
